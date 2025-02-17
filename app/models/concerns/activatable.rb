281627b0ff7b434c9bab4c405fcccd72e9e848fc
phbrugnolo <phbrugnolo@gmail.com>
2025-02-17 14:01:27 -0300

    Concern activatable

---
+++ app/models/concerns/activatable.rb
@@ -0,0 +1,20 @@
+module Activatable
+  extend ActiveSupport::Concern
+
+  included do
+    str_enum :status, %w[active inactive],
+             accessor_methods: true,
+             allow_nil: false,
+             default: true,
+             prefix: false,
+             scopes: true,
+             suffix: false,
+             update_methods: true,
+             validate: true
+
+    scope :active, -> { where status: :active }
+    scope :inactive, -> { where status: :inactive }
+
+    validates :status, presence: true
+  end
+end
