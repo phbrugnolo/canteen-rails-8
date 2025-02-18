f5904630bd06c52fe7d3cf2bc3c5854c1931737a
phbrugnolo <phbrugnolo@gmail.com>
2025-02-18 12:56:03 -0300

    script recreate_database

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,50 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Product.create([
+  {
+    name: "Bala",
+    description: "7belo",
+    status: "active",
+    price: 0.25
+  },
+  {
+    name: "RTX 4090",
+    description: "roda td",
+    status: "inactive",
+    price: 13000
+  },
+  {
+    name: "Diversos",
+    description: "diverso",
+    status: "active",
+    price: 0.50
+  }
+])
+
+Customer.create([
+  {
+    name: "João Nome Muito Grande Grande Grande Grande",
+    matriculation: "123456",
+    status: "active"
+  },
+  {
+    name: "Pedro",
+    matriculation: "654321",
+    status: "active"
+  },
+  {
+    name: "Teste",
+    matriculation: "1456789",
+    status: "inactive"
+  }
+])
+
+Sale.create([
+  {
+    customer_id: 1,
+    cart: "[{\"name\":\"Bala\",\"price\":\"0.25\",\"id\":1,\"quantity\":4},{\"name\":\"Diversos\",\"price\":\"0.50\",\"id\":2,\"quantity\":2}]",
+    total_price: 2.00
+  }
+])
---
+++ script/recreate_database.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Saída no caso de qualquer comando falhar
+set -e
+
+# Roda dentro do ambiente de desenvolvimento
+RAILS_ENV=development
+
+# Mensagens informativas
+echo "Dropping the database..."
+bin/rails db:drop RAILS_ENV=$RAILS_ENV
+
+echo "Creating the database..."
+bin/rails db:create RAILS_ENV=$RAILS_ENV
+
+echo "Migrating the database..."
+bin/rails db:migrate RAILS_ENV=$RAILS_ENV
+
+echo "Seeding the database..."
+bin/rails db:seed RAILS_ENV=$RAILS_ENV
+
+echo "Database rebuild completed!"
