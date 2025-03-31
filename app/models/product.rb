6aa4c52b0c8968e9ad6b752b83b35309edf0e6b2
phbrugnolo <phbrugnolo@gmail.com>
2025-03-31 15:55:03 -0300

    Ajustes de locale

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,9 @@
 class Product < ApplicationRecord
+  has_one_attached :image
+
   include Activatable
 
   validates :name, presence: true
   validates :description, presence: true, length: { maximum: 50 }
-  validates :price, presence: true, numericality: true
+  validates :price, presence: true, numericality: { greater_than: 0, less_than_or_equal_to: Float::MAX }
 end
