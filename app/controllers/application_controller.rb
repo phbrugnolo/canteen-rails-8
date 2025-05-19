9f4ad4d7eb58f10bfd82c5a43a952cd1e794e0ee
phbrugnolo <phbrugnolo@gmail.com>
2025-05-19 16:35:36 -0300

    removing extra whitespaces

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_in,    keys: [ :name, :password ])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [ :name, :password ])
   end
 end
