9324dfa95b5005eae7f8239f46b6d8b08df4ad37
jlcomar <joaolucascomar@gmail.com>
2025-06-03 16:23:37 -0300

    numeros em texto

--- Gemfile
+++ Gemfile
@@ -25,6 +25,7 @@ gem "devise"
 gem "str_enum"
 gem 'wicked_pdf'
 gem 'wkhtmltopdf-binary'
+gem 'numbers_and_words'
 
 # Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
 # gem "bcrypt", "~> 3.1.7"
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,26 @@
 module ApplicationHelper
+  def number_to_words(value)
+    integer_part = value.to_i
+    fractional_part = ((value - integer_part) * 100).round
+
+    # Correção para casos de arredondamento
+    if fractional_part >= 100
+      integer_part += 1
+      fractional_part = 0
+    end
+
+    integer_words = integer_part.to_words(locale: :'pt-BR').upcase
+    fractional_words = fractional_part.to_words(locale: :'pt-BR').upcase
+
+    integer_unit = integer_part == 1 ? 'REAL' : 'REAIS'
+    fractional_unit = fractional_part == 1 ? 'CENTAVO' : 'CENTAVOS'
+
+    if integer_part == 0 && fractional_part > 0
+      "#{fractional_words} #{fractional_unit}"
+    elsif fractional_part == 0
+      "#{integer_words} #{integer_unit}"
+    else
+      "#{integer_words} #{integer_unit} E #{fractional_words} #{fractional_unit}"
+    end
+  end
 end
